ecd3b518596e42e86f0f5e500b43981a1b0bfd73
borkovski <borkovski@o2.pl>
2016-02-05 09:26:00 +0100

    Update Program.cs
    Test comment added

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("Test");
+            //test comment
             Console.ReadKey();
         }
     }
